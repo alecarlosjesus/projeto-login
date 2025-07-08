59870ef8eddd837fa939aa96cf9438d75b87698e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-07-08 00:59:57 -0300

    verificando valor do resultado do cadastro

--- controllers/usuario-controller.js
+++ controllers/usuario-controller.js
@@ -31,7 +31,7 @@ export async function cadastroUsuarioPost(req, res) {
   const { email, senha } = req.body;
 
   const resultado = await inserirUsuario({ email, senha });
-
+  alert(resultado);
   if (resultado) {
     res.redirect('/login');
   } else {
